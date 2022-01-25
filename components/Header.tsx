58c0b99221e07ecada020d8d72806783683da7e1
muhammad-akif <muhammadakif2917@gmail.com>
2022-01-25 18:28:51 +0500

    completed header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 const Header = () => {
   return (
-  <header>
+  <header className="flex justify-between p-5 max-w-6xl mx-auto">
       <div className="flex items-center space-x-5">
           <Link href="/">
              <img 
@@ -15,8 +15,11 @@ const Header = () => {
               <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">Follow</h3>
           </div>
       </div>
-      <div>
-        
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sign In</h3>
+        <h3 className="border border-green-600 px-4 py-1 rounded-full">
+            Get Started
+        </h3>
       </div>
   </header>
   );
