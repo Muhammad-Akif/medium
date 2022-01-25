d3faff1efdb8c15bb9fd28b3ff08f6f6bed22b95
muhammad-akif <muhammadakif2917@gmail.com>
2022-01-25 18:38:26 +0500

    added basic html for banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <div>
+        <div>
+          <h1>
+            <span>Medium</span>{" "}
+            is a place to wite, read, and connect.
+          </h1>
+          <h2>
+            It's easy and free to post your thinking on any topic and connect with millions of readers.
+          </h2>
+        </div>
+      </div>
     </div>
   )
 }
