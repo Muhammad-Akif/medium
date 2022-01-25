84f5009a576f33f26e35e9db79d7108ab4c051d6
muhammad-akif <muhammadakif2917@gmail.com>
2022-01-25 21:15:24 +0500

    banner styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,26 +3,27 @@ import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="max-w-6xl mx-auto">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <div>
-        <div>
-          <h1>
-            <span>Medium</span>{" "}
+      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+        <div className="px-10 space-x-5">
+          <h1 className="text-6xl max-w-xl font-serif">
+            <span className="underline decoration-black decoration-4">Medium</span>{" "}
             is a place to wite, read, and connect.
           </h1>
           <h2>
             It's easy and free to post your thinking on any topic and connect with millions of readers.
           </h2>
         </div>
+        <img
+          className="hidden md:inline-flex h-32 lg:h-full"
+          src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+          alt="logo2" />
       </div>
-      <img 
-      src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" 
-      alt="logo2" />
     </div>
   )
 }
