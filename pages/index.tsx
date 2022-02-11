5a0ef36018bcd0512ab5fde3ecd18279f61adabc
muhammad-akif <muhammadakif2917@gmail.com>
2022-02-11 20:47:15 +0500

    Posts Image added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Header from '../components/Header'
-import { sanityClient } from '../sanity'
+import { sanityClient, urlFor } from '../sanity'
 import { post } from '../typings'
 
-interface Props{
+interface Props {
   posts: [post];
 }
 
 export default function Home({ posts }: Props) {
-  console.log("Posts --> ",posts);
+  console.log("Posts --> ", posts);
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -33,12 +33,18 @@ export default function Home({ posts }: Props) {
           alt="logo2" />
       </div>
       {/* Posts */}
-      
+
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
         {
           posts.map((post) => (
             <Link key={post._id} href={`/post/${post.slug.current}`}>
-            abc
+              <div>
+                <img 
+                className="w-full h-60 object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                src={urlFor(post.mainImage).url()!} 
+                alt="Post Image" 
+                />
+              </div>
             </Link>
           ))
         }
