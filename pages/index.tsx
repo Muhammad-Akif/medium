0a6a1e470456f783fa059dc01b147a1039a14b42
muhammad-akif <muhammadakif2917@gmail.com>
2022-02-11 21:33:01 +0500

    cards completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,11 +39,24 @@ export default function Home({ posts }: Props) {
           posts.map((post) => (
             <Link key={post._id} href={`/post/${post.slug.current}`}>
               <div className="group border rounded-lg cursor-pointer overflow-hidden">
-                <img 
-                className="h-60 object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
-                src={urlFor(post.mainImage).url()!} 
-                alt="Post Image" 
+                <img
+                  className="h-60 object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                  src={urlFor(post.mainImage).url()!}
+                  alt="Post Image"
                 />
+                <div className="flex justify-between p-5 bg-white">
+                  <div>
+                    <p className="text-lg font-bold">{post.title}</p>
+                    <p className="text-xs">
+                      {post.description} by {post.author.name}
+                    </p>
+                  </div>
+                  <img
+                    className="h-12 w-12 rounded-full"
+                    src={urlFor(post.author.image).url()!}
+                    alt="author image"
+                  />
+                </div>
               </div>
             </Link>
           ))
