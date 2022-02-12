696d9528ccd319aa782d4955e581366308e84de4
muhammad-akif <muhammadakif2917@gmail.com>
2022-02-12 20:50:55 +0500

    created new dynamic route for post

---
+++ pages/post/[slug].tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../components/Header'
+import { sanityClient, urlFor } from "../../sanity";
+import { post } from "../../typings"
+
+const Post = () => {
+  return (
+    <div>Post</div>
+  )
+}
+
+export default Post
