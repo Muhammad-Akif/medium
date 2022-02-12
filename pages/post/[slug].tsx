456592d193d1eba65d8a771ec1d033e11652aa64
muhammad-akif <muhammadakif2917@gmail.com>
2022-02-12 21:00:16 +0500

    done with getStaticPaths

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -4,9 +4,24 @@ import { sanityClient, urlFor } from "../../sanity";
 import { post } from "../../typings"
 
 const Post = () => {
-  return (
-    <div>Post</div>
-  )
+    return (
+        <div>Post</div>
+    )
 }
 
-export default Post
+export default Post;
+
+export const getStaticPaths = async () => {
+    const query = `*[_type == "post"]{
+        _id,
+        slug {
+          current
+        }
+      }`
+    const posts = await sanityClient.fetch(query);
+    const paths = posts.map((post: post) => ({
+        params: {
+            slug: post.slug.current,
+        }
+    }))
+}
