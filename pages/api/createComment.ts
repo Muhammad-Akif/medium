8b169f0bb296016e0f6bf087fef3131d9becd130
muhammad-akif <muhammadakif2917@gmail.com>
2022-02-13 21:24:02 +0500

    done with the api/createComment

--- pages/api/createComment.ts
+++ pages/api/createComment.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import sanityClient from '@sanity/client'
 
 const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
@@ -7,9 +8,30 @@ const config = {
     token: process.env.SANITY_API_TOKEN
 }
 
-export default function createComment(
-  req: NextApiRequest,
-  res: NextApiResponse
+const client = sanityClient(config);
+
+export default async function createComment(
+    req: NextApiRequest,
+    res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+    const { _id, name, email, comment } = JSON.parse(req.body);
+    
+    try {
+        await client.create({
+            _type: "comment",
+            post:{
+                _type: "reference",
+                _ref: _id,
+            },
+            name,
+            email,
+            comment,
+        })
+    }
+    catch (err){
+        return res.status(500).json({ message: "Commit Not Submitted", err });
+    }
+    console.log("comment submitted")
+    return res.status(200).json({ message: 'Comment Submitted' })
+
 }
