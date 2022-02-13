e3046ee5db29d727c0e12fdbd005c7df220598ef
muhammad-akif <muhammadakif2917@gmail.com>
2022-02-13 21:02:53 +0500

    added config obj and api token

--- pages/api/createComment.ts
+++ pages/api/createComment.ts
@@ -1,5 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+const config = {
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    useCdn: process.env.NODE_ENV === "production",
+    token: process.env.SANITY_API_TOKEN
+}
+
 export default function createComment(
   req: NextApiRequest,
   res: NextApiResponse
