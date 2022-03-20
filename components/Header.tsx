1c66a153207be1af49bf16a8c32494f37df23aeb
muhammad-akif <muhammadakif2917@gmail.com>
2022-03-20 16:10:59 +0500

    updated nav responsiveness and push route

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,19 @@
-import Link from 'next/link'
+// import Link from 'next/link'
+import { useRouter } from 'next/router'
+
 const Header = () => {
+  const router = useRouter();
+
   return (
   <header className="flex justify-between p-5">
       <div className="flex items-center space-x-5">
-          <Link href="/">
+          {/* <Link href="/"> */}
              <img 
+             onClick={() => router.push(`/`)}
              className="w-44 object-contain cursor-pointer"
              src="https://links.papareact.com/yvf" 
              alt="logo" />
-          </Link>
+          {/* </Link> */}
           <div className="hidden md:inline-flex items-center space-x-5">
               <h3>About</h3>
               <h3>Contact</h3>
@@ -16,7 +21,7 @@ const Header = () => {
           </div>
       </div>
       <div className="flex items-center space-x-5 text-green-600">
-        <h3>Sign In</h3>
+        <h3 className="hidden sm:inline-block">Sign In</h3>
         <h3 className="border border-green-600 px-4 py-1 rounded-full">
             Get Started
         </h3>
